6a7610c215fc47f7c23c6e137b9c2a65d374c837
nat123asha <natasha@fairleylaw.co.nz>
2026-03-02 17:14:59 +1300

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -35,7 +35,11 @@ function Main() {
             </a> */}
           </div>
           <h1>Natasha Fairley</h1>
-          <p>Family Lawyer</p>
+          <h3>Barrister</h3>
+          <p style={{marginTop:"40px"}}>Natasha has over 10 years of court experience in the Family Court. Natasha has also represented clients in the High Court and appeared at the Court of Appeal.
+Specialising in parenting and guardianship disputes under the Care of Children Act 2004, Oranga Tamariki Act 1989 proceedings, and Family Violence Act 2018 proceedings, including applications for and responding to protection order applications.
+Natasha also deals with matters relating to mental health under the Mental Health (Compulsory Assessment and Treatment) Act 1992 and appointments of welfare guardians and property managers under the Protection of Personal and Property Rights Act 1988.
+A legal aid provider for over seven years, Natasha can assist clients on a private basis or who require legal aid for legal representation.</p>
 
           <div className="mobile_social_icons">
             {/* <a
@@ -60,3 +64,4 @@ function Main() {
 }
 
 export default Main;
+
